a2eb3c5d436982f02b195f57eb1e87e115688279
Hyeok Jun <yuwolxx@gmail.com>
2025-07-29 16:57:53 +0900

    dd

---
+++ 01_python/hw/01_hw/python_ws_1_a/ws_1_a.py
@@ -0,0 +1,19 @@
+# 아래에 코드를 작성하시오.
+# 요구사항
+# name 변수에 'Alice' 문자열을 할당한다.
+name = 'Alice'
+
+# age 변수에 25 정수를 할당한다.
+age = 25
+
+# height 변수에 5.6 부동 소수점을 할당한다.
+height = 5.6
+
+# is_student 변수에 True 불린 값을 할당한다.
+is_student = True
+
+# 각 변수에 담긴 값을 출력한다.
+# f-string을 활용하여 'Alice는 25살이고, 키는 5.6이며 학생 여부는 True입니다.' 문자열을 출력한다.
+print(f"({name} is {age} years old, height is {height} and student is {is_student})")
+
+# 단, name, age, height, is_student 변수를 사용하여야 한다.
