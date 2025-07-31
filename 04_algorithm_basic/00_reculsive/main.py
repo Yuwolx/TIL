b3bc756d60157a2a8c5462542282224c175584d3
Hyeok Jun <yuwolxx@gmail.com>
2025-07-31 11:29:00 +0900

    dd

---
+++ 04_algorithm_basic/00_reculsive/main.py
@@ -0,0 +1,5 @@
+n = int(input())
+
+def stars(n):
+    if n//3 == 1:
+        stars_list = [* for
---
+++ 04_algorithm_basic/01-algorithm-basic-1/07_30/colored/colored.py
@@ -0,0 +1,5 @@
+import sys
+# open input text file
+sys.stdin = open('sample_input.txt')
+
+T = int(input())
