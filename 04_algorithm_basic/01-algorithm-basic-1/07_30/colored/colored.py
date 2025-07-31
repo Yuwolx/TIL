aa44e1801827927d2ce0799277e655face9fe4ca
Hyeok Jun <yuwolxx@gmail.com>
2025-07-31 15:59:50 +0900

    dd

--- 04_algorithm_basic/01-algorithm-basic-1/07_30/colored/colored.py
+++ 04_algorithm_basic/01-algorithm-basic-1/07_30/colored/colored.py
@@ -3,3 +3,25 @@ import sys
 sys.stdin = open('sample_input.txt')
 
 T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+
+    total_list = []
+    for x in range(10):
+        blank_list = [ 0 for n in range(10)]
+        total_list.append(blank_list)
+
+    for _ in range(N):
+        r1, c1, r2, c2, color = map(int, input().split())
+
+        for x in range(r1,r2+1):
+            for y in range(c1,c2+1):
+                total_list[x][y] += color
+
+    results = 0
+    for x in range(10):
+        for y in range(10):
+            if total_list[x][y] == 3:
+                results += 1
+
+    print(f"#{tc} {results}")
---
+++ 04_algorithm_basic/07_31/ex.py
@@ -0,0 +1,5 @@
+relative_location = [2,0,-2,-4,-6]
+continued = [2,0]
+rest_stop = relative_location[len(continued):]
+
+print(rest_stop)
