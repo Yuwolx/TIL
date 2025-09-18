e334c91c771170e6fde81ba4659ba62e19548817
Hyeok Jun <yuwolxx@gmail.com>
2025-09-18 22:03:22 +0900

    dd

--- baekjoon/1697.py
+++ baekjoon/1697.py
@@ -1,5 +1,5 @@
 n, k = map(int,input().split())
-print(n,k)
+print(f'n,k: {n,k}')
 
 def one_point(n):
     return n + 1
@@ -9,3 +9,30 @@ def minus_point(n):
 
 def double(n):
     return 2*n
+
+# case1 k < n
+if k < n:
+    c1 = k-n
+
+#case2 need +
+c2 = n - k
+
+
+t = 0
+# case3 need x
+n3 = n
+t3 = t
+while n3 < k:
+    n3 = double(n3)
+    t3 += 1
+    if n3 == k:
+        print(t3)
+        break
+
+#case4 need - * x
+n4 = n
+t4 = t
+
+#case5 need + * x
+n5 = int(n/2)
+t5 = t-5
